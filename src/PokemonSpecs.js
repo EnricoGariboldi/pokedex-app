17e1a9019fb36f67b5fcac573f7dd32dc82f22a3
EnricoGariboldi <you@example.com>
2022-01-26 16:23:53 +0100

    final fix (pokemon attributes name added)

--- src/PokemonSpecs.js
+++ src/PokemonSpecs.js
@@ -4,13 +4,13 @@ import { Grid } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { Stack } from "@mui/material";
-import LinearProgressWithLabel from "./Components/ProgressBar/ProgressBar"
-
+import LinearProgressWithLabel from "./Components/ProgressBar/ProgressBar";
 
 const PokemonSpecs = () => {
   let navigate = useNavigate();
   const location = useLocation();
 
+  console.log(location);
 
   const redirect = () => {
     navigate("/", { state: null });
@@ -18,8 +18,6 @@ const PokemonSpecs = () => {
 
   return (
     <div className="PokemonSpecs">
-      
-
       <div className="PokemonSpecs-backToPokedex">
         <Stack direction="row" justifyContent="center">
           <Button
@@ -34,7 +32,7 @@ const PokemonSpecs = () => {
       </div>
 
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={6}>  
+        <Grid item xs={12} sm={6} md={6}>
           <div className="PokemonSpecs-poke-container">
             <div className="PokemonSpecs-img-container">
               <img alt="pokemon" src={location.state.img} />
@@ -42,79 +40,62 @@ const PokemonSpecs = () => {
             <div className="PokemonSpecs-poke-name">{location.state.name}</div>
           </div>
 
-      <br></br>
+          <br></br>
 
           <Grid item xs={12} sm={12} md={12}>
-        <div className="PokemonSpecs-specs-bar">
-          <div className="PokemonSpecs-specs-title">
-            Stats:
+            <div className="PokemonSpecs-specs-bar">
+              <div className="PokemonSpecs-specs-title">Stats:</div>
+              <div className="PokemonsSpecs-specs-attributes">
+                Hp / Attack / Defense / Special Attack / Special Defense / Speed
+              </div>
+              <div className="PokemonSpecs-specs-data">
+                {location.state.stats.map((element) => {
+                  return (
+                    <div>
+                      <LinearProgressWithLabel progress={element} />
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-            <div className="PokemonSpecs-specs-data">
-              {
-                
-                location.state.stats.map( element => {
-                  return <div><LinearProgressWithLabel progress={element}  /></div>
-                } )
-                
-                  }
-            </div>
-
-        
-
-          </div>
-        </Grid>
-
-
+          </Grid>
         </Grid>
 
         <Grid item xs={12} sm={6} md={6}>
-        <div className="PokemonSpecs-specs-container">
-
-        <div className="PokemonSpecs-specs-section">
-          <div className="PokemonSpecs-specs-title">
-            Height:
-            </div>
-            <div className="PokemonSpecs-specs-data">{location.state.height}
-            </div>
-            </div>
-            
+          <div className="PokemonSpecs-specs-container">
             <div className="PokemonSpecs-specs-section">
-          <div className="PokemonSpecs-specs-title">
-            Weight:
-            </div>
-            <div className="PokemonSpecs-specs-data">{location.state.weight}
-            </div>
+              <div className="PokemonSpecs-specs-title">Height:</div>
+              <div className="PokemonSpecs-specs-data">
+                {location.state.height}
+              </div>
             </div>
 
             <div className="PokemonSpecs-specs-section">
-          <div className="PokemonSpecs-specs-title">
-            Category:
-            </div>
-            <div className="PokemonSpecs-specs-data">
-              {
-                location.state.types.map( (element) => {
-                  return  <div >{element}</div>
-                } )
-              } 
-            </div>
+              <div className="PokemonSpecs-specs-title">Weight:</div>
+              <div className="PokemonSpecs-specs-data">
+                {location.state.weight}
+              </div>
             </div>
 
             <div className="PokemonSpecs-specs-section">
-          <div className="PokemonSpecs-specs-title">
-            Abilities:
+              <div className="PokemonSpecs-specs-title">Category:</div>
+              <div className="PokemonSpecs-specs-data">
+                {location.state.types.map((element) => {
+                  return <div>{element}</div>;
+                })}
+              </div>
             </div>
-            <div className="PokemonSpecs-specs-data">
-              {
-                location.state.abilities.map( (element) => {
-                  return <div>{element}</div>
-                } )
-               }
+
+            <div className="PokemonSpecs-specs-section">
+              <div className="PokemonSpecs-specs-title">Abilities:</div>
+              <div className="PokemonSpecs-specs-data">
+                {location.state.abilities.map((element) => {
+                  return <div>{element}</div>;
+                })}
+              </div>
             </div>
-            </div>
-            </div>
+          </div>
         </Grid>
-
-
       </Grid>
     </div>
   );
